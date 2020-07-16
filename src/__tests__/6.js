cff5998f0f245d4f543d587172acfe1af7362c33
yonatan bendahan <yonatan.bendahan@gmail.com>
2020-07-16 15:42:02 +0300

    added two more exercises

---
+++ src/__tests__/6.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import ListOfProducts from '../exercises/1_ListOfProducts';
+
+it('should have three products', () => {
+  const products = [
+    { id: '1', name: 'a', price: 12 },
+    { id: '2', name: 'b', price: 14 },
+    { id: '3', name: 'c' }
+  ]
+  const { getByText, container } = render(<ListOfProducts products={products} />);
+  expect(container.querySelectorAll('li').length).toBe(products.length);
+  products.forEach(product => {
+    if (product.price) {
+      const li = getByText(`${product.name} ${product.price}`);
+      expect(li).toBeDefined();
+      expect(li.tagName.toLowerCase()).toBe('li');
+    } else {
+      const li = getByText(`${product.name} N/A`);
+      expect(li).toBeDefined();
+      expect(li.tagName.toLowerCase()).toBe('li');
+    }
+  })
+});
+
+
+it('should have random number product', () => {
+  const products = Array(Math.floor(Math.random() * 100))
+    .fill(1)
+    .map((number, index) => ({ id: index + '', name: index + ' product', price: number }));
+  const { getByText, container } = render(<ListOfProducts products={products} />);
+  expect(container.querySelectorAll('li').length).toBe(products.length);
+  products.forEach(product => {
+    if (product.price) {
+      const li = getByText(`${product.name} ${product.price}`);
+      expect(li).toBeDefined();
+      expect(li.tagName.toLowerCase()).toBe('li');
+    } else {
+      const li = getByText(`${product.name} N/A`);
+      expect(li).toBeDefined();
+      expect(li.tagName.toLowerCase()).toBe('li');
+    }
+  })
+});
---
+++ src/exercises/7_WithChildren.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+/**
+ * Implement a function component that accepts children in props
+ * and render them inside a div
+ */
+export default function WithChildren(props) {
+  // Your code
+}
